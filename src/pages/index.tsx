1fa14f3553fafa46ecc0e9ba94e852d2ae28a889
Tom Heaton <thomas.m.heaton@gmail.com>
2022-11-14 21:19:31 +0000

    new homepage stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import ModWidgets from "@site/src/components/ModWidgets";
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import styles from './index.module.css';
 
 function HomepageHeader(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+    const {siteConfig} = useDocusaurusContext();
 
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">
-            {siteConfig.title}
-        </h1>
-        <p className="hero__subtitle">
-            {siteConfig.tagline}
-        </p>
-        {/*<div className={styles.buttons}>
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">
+                    {siteConfig.title}
+                </h1>
+                <p className="hero__subtitle">
+                    {siteConfig.tagline}
+                </p>
+                {/*<div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro"
@@ -27,29 +25,36 @@ function HomepageHeader(): JSX.Element {
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>*/}
-      </div>
-    </header>
-  );
+            </div>
+        </header>
+    );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+    const {siteConfig} = useDocusaurusContext();
 
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-      <main>
-        {/*<HomepageFeatures />*/}
-        <p>
-          Hello, I'm McJty.
-          I'm a Minecraft mod developer, modpack developer and YouTuber!
-          I've made several mods for your and my pleasure and also like to help out other members in the community.
-          I'm also a member of the popular server group ForgeCraft.
-        </p>
-      </main>
-    </Layout>
-  );
+    return (
+        <Layout
+            title={`${siteConfig.title}`}
+            description="Description will go into a meta tag in <head />"
+        >
+            <HomepageHeader/>
+            <main>
+                {/*<HomepageFeatures />*/}
+                <div className={styles.mcjtyInfo}>
+                    <img src={require("@site/static/img/mcjty.png").default} alt={"mcjty logo"}/>
+                    <p>
+                        Hello, I'm McJty.
+                        I'm a Minecraft mod <strong>developer, modpack developer</strong> and <strong>YouTuber!</strong>
+                        <br/>
+                        I've made several mods for your and my pleasure and also like to help out other members in the
+                        community.
+                        <br/>
+                        I'm also a member of the popular server group <strong>ForgeCraft</strong>.
+                    </p>
+                </div>
+                <ModWidgets/>
+            </main>
+        </Layout>
+    );
 }
