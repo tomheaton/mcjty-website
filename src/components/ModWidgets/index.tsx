b49c8224927be3e4a65cdf199eac5f6f3ceee7d2
Tom Heaton <thomas.m.heaton@gmail.com>
2022-12-05 04:37:18 +0000

    cleanup

--- src/components/ModWidgets/index.tsx
+++ src/components/ModWidgets/index.tsx
@@ -1,37 +1,37 @@
-import React, {useEffect, useState} from 'react';
-import styles from './styles.module.css';
+import React, {useEffect, useState} from "react";
+import styles from "./styles.module.css";
 
 const ModWidget: React.FC<{id: number}> = ({id}) => {
-    return (
-        <div className={styles.modWidget}>
-            <iframe
-                src={`https://www.cfwidget.com/${id}`}
-                width={"100%"}
-                style={{border: "none"}}
-            />
-        </div>
-    );
-}
+  return (
+    <div className={styles.modWidget}>
+      <iframe
+        src={`https://www.cfwidget.com/${id}`}
+        width={"100%"}
+        style={{border: "none"}}
+      />
+    </div>
+  );
+};
 
 const ModWidgets: React.FC = () => {
-    const [mods, setMods] = useState<number[]>([]);
+  const [mods, setMods] = useState<number[]>([]);
 
-    useEffect(() => {
-        fetch("https://api.cfwidget.com/author/search/mcjty")
-            .then(res => res.json())
-            .then(data => setMods(data.projects?.map(m => m?.id)))
-    }, []);
+  useEffect(() => {
+    fetch("https://api.cfwidget.com/author/search/mcjty")
+      .then(res => res.json())
+      .then(data => setMods(data.projects?.map(m => m?.id)));
+  }, []);
 
-    return (
-        <section className={styles.container}>
-            <h2>
-                Mods
-            </h2>
-            <div className={styles.modWidgetContainer}>
-                {mods.map((id) => <ModWidget key={id} id={id}/>)}
-            </div>
-        </section>
-    );
-}
+  return (
+    <section className={styles.container}>
+      <h2>
+        Mods
+      </h2>
+      <div className={styles.modWidgetContainer}>
+        {mods.map((id) => <ModWidget key={id} id={id} />)}
+      </div>
+    </section>
+  );
+};
 
 export default ModWidgets;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import clsx from 'clsx';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import React from "react";
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import ModWidgets from "@site/src/components/ModWidgets";
-import Layout from '@theme/Layout';
-import styles from './index.module.css';
+import Layout from "@theme/Layout";
+import styles from "./index.module.css";
 import Link from "@docusaurus/Link";
 
 const HomepageHeader: React.FC = () => {
-    const {siteConfig} = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
 
-    return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
-            <div className={"container"}>
-                <h1 className={"hero__title"}>
-                    {siteConfig.title}
-                </h1>
-                <p className={"hero__subtitle"}>
-                    {siteConfig.tagline}
-                </p>
-                {/*<div className={styles.buttons}>
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className={"container"}>
+        <h1 className={"hero__title"}>
+          {siteConfig.title}
+        </h1>
+        <p className={"hero__subtitle"}>
+          {siteConfig.tagline}
+        </p>
+        {/*<div className={styles.buttons}>
                     <Link
                         className={"button button--secondary button--lg"}
                         to={"/docs/intro"}
@@ -26,49 +26,49 @@ const HomepageHeader: React.FC = () => {
                         Docusaurus Tutorial - 5min ⏱️
                     </Link>
                 </div>*/}
-            </div>
-        </header>
-    );
-}
+      </div>
+    </header>
+  );
+};
 
 const Home: React.FC = () => {
-    const {siteConfig} = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
 
-    return (
-        <Layout title={siteConfig.title} description={siteConfig.tagline}>
-            <HomepageHeader/>
-            <main>
-                {/*<HomepageFeatures />*/}
-                {/*TODO: fix mobile styling*/}
-                <div className={styles.mcjtyInfo}>
-                    <img src={require("@site/static/img/mcjty.png").default} alt={"McJty Logo"}/>
-                    <p className={styles.text}>
-                        Hello, I'm McJty.
-                        I'm a Minecraft mod <strong>developer, modpack developer</strong> and <strong>YouTuber!</strong>
-                        <br/>
-                        I've made several mods for your and my pleasure and also like to help out other members in the
-                        community.
-                        <br/>
-                        I'm also a member of the popular server group <strong>ForgeCraft</strong>.
-                    </p>
-                </div>
-                {/*TODO: fix mobile styling*/}
-                <ModWidgets/>
-                {/*TODO: fix mobile styling*/}
-                <div className={styles.romeloInfo}>
-                    <p className={styles.text} style={{textAlign: "center"}}>
-                        Did you know McJty&apos;s son makes mods too?
-                    </p>
-                    <Link
-                        className={"button button--primary button--lg"}
-                        href={"https://www.curseforge.com/members/romelo333/projects"}
-                    >
-                        Romelo's Mods
-                    </Link>
-                </div>
-            </main>
-        </Layout>
-    );
-}
+  return (
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
+      <HomepageHeader />
+      <main>
+        {/*<HomepageFeatures />*/}
+        {/*TODO: fix mobile styling*/}
+        <div className={styles.mcjtyInfo}>
+          <img src={require("@site/static/img/mcjty.png").default} alt={"McJty Logo"} />
+          <p className={styles.text}>
+            Hello, I'm McJty.
+            I'm a Minecraft mod <strong>developer, modpack developer</strong> and <strong>YouTuber!</strong>
+            <br />
+            I've made several mods for your and my pleasure and also like to help out other members in the
+            community.
+            <br />
+            I'm also a member of the popular server group <strong>ForgeCraft</strong>.
+          </p>
+        </div>
+        {/*TODO: fix mobile styling*/}
+        <ModWidgets />
+        {/*TODO: fix mobile styling*/}
+        <div className={styles.romeloInfo}>
+          <p className={styles.text} style={{textAlign: "center"}}>
+            Did you know McJty&apos;s son makes mods too?
+          </p>
+          <Link
+            className={"button button--primary button--lg"}
+            href={"https://www.curseforge.com/members/romelo333/projects"}
+          >
+            Romelo's Mods
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  );
+};
 
 export default Home;
