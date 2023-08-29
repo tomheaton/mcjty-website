3094b3351594d7f611549296ba428d102466431a
Tom Heaton <thomas.m.heaton@gmail.com>
2023-08-29 18:57:49 +0100

    prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+    trailingComma: "all",
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    bracketSpacing: true,
+    printWidth: 80,
+    endOfLine: "lf",
+};
+
+module.exports = config;
