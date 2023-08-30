0e1fbc2a61a28e9d6c56946c4be7ec481739a732
McJty <mcjty1@gmail.com>
2023-08-30 14:59:17 +0200

    First schema attempt

--- www/src/components/ControlValidator/data.ts
+++ www/src/components/ControlValidator/data.ts
@@ -1,8 +1,7 @@
-import { spawnSchema } from "./schemas";
+import {spawnSchema1_20, spawnSchema1_19, spawnerSchema, phasesSchema} from "./schemas";
 import z from "zod";
 
 export const MINECRAFT_VERSIONS = [
-  "1.12.2",
   "1.16.5",
   "1.18.2",
   "1.19.2",
@@ -17,13 +16,24 @@ type DataType = Partial<
   Record<MinecraftVersion, Partial<Record<ValidatorType, z.ZodSchema<any>>>>
 >;
 export const DATA: DataType = {
-  "1.12.2": {},
-  "1.16.5": {},
-  "1.18.2": {},
-  "1.19.2": {},
+  "1.16.5": {
+    spawn: spawnSchema1_19,
+    spawner: spawnerSchema,
+    phases: phasesSchema,
+  },
+  "1.18.2": {
+    spawn: spawnSchema1_19,
+    spawner: spawnerSchema,
+    phases: phasesSchema,
+  },
+  "1.19.2": {
+    spawn: spawnSchema1_19,
+    spawner: spawnerSchema,
+    phases: phasesSchema,
+  },
   "1.20": {
-    spawn: spawnSchema,
-    spawner: spawnSchema,
-    phases: spawnSchema,
+    spawn: spawnSchema1_20,
+    spawner: spawnerSchema,
+    phases: phasesSchema,
   },
 } as const;
