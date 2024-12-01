6f3ecf523dfe14207bb52e4f03c09b02a77f1053
Tom Heaton <thomas.m.heaton@gmail.com>
2024-12-01 10:14:00 +0000

    feat: redirector base url

--- redirector/next.config.ts
+++ redirector/next.config.ts
@@ -2,21 +2,26 @@ import type { NextConfig } from "next";
 
 export default {
   reactStrictMode: true,
-  // redirects: async () => [
-  //   {
-  //     source: "/:path*",
-  //     destination: "https://www.mcjty.eu/",
-  //     permanent: true,
-  //   },
-  //   {
-  //     source: "/modding/:path*",
-  //     destination: "https://www.mcjty.eu/docs/intro/",
-  //     permanent: true,
-  //   },
-  //   {
-  //     source: "/mods/:path*",
-  //     destination: "https://www.mcjty.eu/docs/mods/",
-  //     permanent: true,
-  //   },
-  // ],
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "https://www.mcjty.eu/",
+      permanent: true,
+    },
+    // {
+    //   source: "/:path*",
+    //   destination: "https://www.mcjty.eu/",
+    //   permanent: true,
+    // },
+    // {
+    //   source: "/modding/:path*",
+    //   destination: "https://www.mcjty.eu/docs/intro/",
+    //   permanent: true,
+    // },
+    // {
+    //   source: "/mods/:path*",
+    //   destination: "https://www.mcjty.eu/docs/mods/",
+    //   permanent: true,
+    // },
+  ],
 } satisfies NextConfig;
